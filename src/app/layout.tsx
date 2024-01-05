83635781079b3927fdf565745eeb271444d24005
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-01-05 17:09:07 -0600

    [ui] added custom scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,12 @@ export default function RootLayout({
                 className={`w-screen h-screen ${inter.className} bg-secondary flex flex-col`}
             >
                 <Navbar />
-                <main className="flex flex-col flex-grow items-center overflow-y-auto">
+                <main className="flex flex-col flex-grow items-center overflow-y-auto custom-scrollbar">
                     <div className="w-full md:w-3/5 max-w-[768px] flex flex-col px-4 md:px-0 py-4">
                         {children}
+                        {[...Array(200)].map((_, i) => (
+                            <p key={i}>something</p>
+                        ))}
                     </div>
                     <div className="w-full h-fit mt-auto">
                         <Footer />
