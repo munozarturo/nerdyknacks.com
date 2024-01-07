d5231eee0217c5921ed297925294ba89bc324da4
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-01-07 16:47:18 -0600

    [api] added permissions

---
+++ db/permissions.sql
@@ -0,0 +1,8 @@
+GRANT EXECUTE ON FUNCTION set_article_tags TO anon;
+GRANT EXECUTE ON FUNCTION set_article_technologies TO anon;
+GRANT EXECUTE ON FUNCTION set_article_links TO anon;
+GRANT EXECUTE ON FUNCTION update_article TO anon;
+GRANT EXECUTE ON FUNCTION delete_article TO anon;
+GRANT EXECUTE ON FUNCTION create_article TO anon;
+GRANT EXECUTE ON FUNCTION get_article TO anon;
+GRANT EXECUTE ON FUNCTION get_all_articles TO anon;
